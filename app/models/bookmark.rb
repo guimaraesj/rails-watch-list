f56de45d7d0b8823f630f61df6f292a16a904a64
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-11-17 12:47:12 +0000

    models created

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  # comment cannot be shorter than 6 characters:
+  validates :comment, length: { minimum: 6 }
+  # is unique for a given movie/list couple:
+  validates_uniqueness_of :movie_id, scope: :list_id
+end

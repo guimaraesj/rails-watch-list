e66668edc9473eaae023f0937637817d3be56d6d
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-11-18 16:27:00 +0000

    add dotenv-rails

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,17 @@
 module ListsHelper
+  # lists helper teve que ser adicionado à mão
+  def image_cloudinary(list)
+    if list.photo.attached?
+      cl_image_path list.photo.key, crop: :fill
+    else
+      image_path "placeholder.jpg"
+    end
+  end
+  def image_cloudinary_tag(list)
+    if list.photo.attached?
+      cl_image_tag list.photo.key
+    else
+      image_tag "placeholder.jpg"
+    end
+  end
 end
